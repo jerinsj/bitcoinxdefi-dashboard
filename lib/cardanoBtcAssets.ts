ca08d828620a214f6ceb2a1406c0e5be2eda3aac
Mantis <jrnjacob@gmail.com>
2026-06-27 19:31:15 -0400

    Create cardanoBtcAssets.ts

---
+++ lib/cardanoBtcAssets.ts
@@ -0,0 +1,18 @@
+export const cardanoBtcAssets = [
+  {
+    symbol: "rsBTC",
+    name: "Rosen Bitcoin",
+    policyId: "2dbc49f682ad21f6d18705cf446f9f7a277731ab70ae21a454f888b2",
+    assetNameHex: "7273425443",
+    decimals: 8,
+    source: "Rosen Bridge",
+  },
+  {
+    symbol: "wanBTC",
+    name: "Wanchain Bitcoin",
+    policyId: "25c5de5f5b286073c593edfd77b48abc7a48e5a4f3d4cd9d428ff935",
+    assetNameHex: "425443",
+    decimals: 8,
+    source: "Wanchain",
+  },
+];
