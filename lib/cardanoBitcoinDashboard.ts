30b411235237fab7ce9323eb0f3e6d1367d34fbf
Mantis <jrnjacob@gmail.com>
2026-06-28 16:43:37 -0400

    Create cardanoBitcoinDashboard.ts

---
+++ lib/cardanoBitcoinDashboard.ts
@@ -0,0 +1,27 @@
+export async function getCardanoBitcoinDashboardData() {
+  const [cardanoRes, karmaRes] = await Promise.all([
+    fetch("https://bitcoinxdefi.com/api/cardano-bitcoin", {
+      next: { revalidate: 604800 },
+    }),
+    fetch("https://bitcoinxdefi.com/api/btc-karma", {
+      next: { revalidate: 604800 },
+    }),
+  ]);
+
+  if (!cardanoRes.ok) {
+    throw new Error("Failed to fetch Cardano Bitcoin data");
+  }
+
+  const cardano = await cardanoRes.json();
+  const karma = karmaRes.ok ? await karmaRes.json() : null;
+
+  return {
+    cardano,
+    karma,
+    totalTrackedBtc:
+      cardano.totalBtc + (karma?.totalBtcStaked ?? 0),
+    protocolCount:
+      cardano.assets.length + (karma ? 1 : 0),
+    updatedAt: cardano.updatedAt,
+  };
+}
