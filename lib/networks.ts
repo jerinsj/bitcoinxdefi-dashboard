579584842218d374d15be213c7d9dfe0f210efff
Mantis <jrnjacob@gmail.com>
2026-06-28 15:25:07 -0400

    Create networks.ts

---
+++ lib/networks.ts
@@ -0,0 +1,12 @@
+export const networks = [
+  {
+    id: "cardano",
+    name: "Cardano",
+    logo: "/logos/cardano.svg",
+  },
+  {
+    id: "bitcoin",
+    name: "Bitcoin",
+    logo: "/logos/bitcoin.svg",
+  },
+];
