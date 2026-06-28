2941325040b53895f639b3079921d43c148f2445
Mantis <jrnjacob@gmail.com>
2026-06-27 21:47:21 -0400

    Update cardanoBtcAssets.ts

--- lib/cardanoBtcAssets.ts
+++ lib/cardanoBtcAssets.ts
@@ -6,6 +6,10 @@ export const cardanoBtcAssets = [
     assetNameHex: "7273425443",
     decimals: 8,
     source: "Rosen Bridge",
+    trackingMethod: "holderAdjusted",
+    excludedAddresses: [
+      "addr1x8x6ca648w25x085dg8xs6k5e69yemr5hakcnl0gshmal6gahwzvy33q3jhr74lurrp9p0n8derw58fh7snq2zwxe8zsdkcqrj",
+    ],
   },
   {
     symbol: "wanBTC",
@@ -14,5 +18,7 @@ export const cardanoBtcAssets = [
     assetNameHex: "425443",
     decimals: 8,
     source: "Wanchain",
+    trackingMethod: "assetSupply",
+    excludedAddresses: [],
   },
-];
+] as const;
